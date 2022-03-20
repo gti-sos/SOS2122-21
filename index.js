32e2b0c4e57170fba5b927d637ee1830dfbd2bdd
antruigon <antoniorg2910@gmail.com>
2022-03-20 22:57:23 +0100

    Añadido GET y POST del recurso in-use-vehicles

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 8080;
+const BASE_API_URL = "/api/v1";
 
+app.use(bodyParser.json());
 app.use("/",express.static('public'));
 
 app.get("/cool", (req,res) => {
@@ -17,4 +20,16 @@ app.get("/cool", (req,res) => {
 
 app.listen(port, () => {
     console.log(`Server ready at port ${port}`)
+});
+
+//Antonio
+var inUseVehicles = []; 
+
+app.get(BASE_API_URL+"/in-use-vehicles", (req,res) => {
+    res.send(JSON.stringify(inUseVehicles,null,2));
+})
+
+app.post(BASE_API_URL+"/in-use-vehicles", (req,res) => {
+    inUseVehicles.push(req.body);
+    res.sendStatus(201,"CREATED")
 });
