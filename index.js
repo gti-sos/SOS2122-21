e7e8d2730b4bd7a64b9f29694d026c8b144630c5
antruigon <antoniorg2910@gmail.com>
2022-03-22 11:35:11 +0100

    In-use-vehicles Initial Data

--- index.js
+++ index.js
@@ -33,3 +33,46 @@ app.post(BASE_API_URL+"/in-use-vehicles", (req,res) => {
     inUseVehicles.push(req.body);
     res.sendStatus(201,"CREATED")
 });
+
+app.get(BASE_API_URL+"/in-use-vehicles/loadInitialData", (req,res) => {
+    var iniData = [
+        {
+            country:  "Spain",
+            year: 2020,
+            veh_use_comm: 4538423,
+            veh_use_pass: 25169158,
+            veh_use_per_1000: 626.76,
+        },
+        {
+            country:  "Germany",
+            year: 2020,
+            veh_use_comm: 4027249,
+            veh_use_pass: 48248584,
+            veh_use_per_1000: 628.66,
+        },
+        {
+            country:  "United Kingdom",
+            year: 2020,
+            veh_use_comm: 5949323,
+            veh_use_pass: 42403988,
+            veh_use_per_1000: 632.65,
+        },
+        {
+            country:  "France",
+            year: 2020,
+            veh_use_comm: 6598185,
+            veh_use_pass: 44944450,
+            veh_use_per_1000: 666.44,
+        },
+        {
+            country:  "Italy",
+            year: 2020,
+            veh_use_comm: 5281807,
+            veh_use_pass: 44999681,
+            veh_use_per_1000: 759.39,
+        }
+    ];
+    inUseVehicles.push(iniData);
+    res.send(JSON.stringify(inUseVehicles,null,2));
+})
+
