6d0534e274d3393db4975a69478b4da292b3ac60
antruigon <antoniorg2910@gmail.com>
2022-03-22 12:19:12 +0100

    GET /in-use-vehicles/country

--- index.js
+++ index.js
@@ -33,35 +33,35 @@ app.get(BASE_API_URL+"/in-use-vehicles", (req,res) => {
 app.get(BASE_API_URL+"/in-use-vehicles/loadInitialData", (req,res) => {
     var iniData = [
         {
-            country:  "Spain",
+            country:  "spain",
             year: 2020,
             veh_use_comm: 4538423,
             veh_use_pass: 25169158,
             veh_use_per_1000: 626.76,
         },
         {
-            country:  "Germany",
+            country:  "germany",
             year: 2020,
             veh_use_comm: 4027249,
             veh_use_pass: 48248584,
             veh_use_per_1000: 628.66,
         },
         {
-            country:  "United Kingdom",
+            country:  "united kingdom",
             year: 2020,
             veh_use_comm: 5949323,
             veh_use_pass: 42403988,
             veh_use_per_1000: 632.65,
         },
         {
-            country:  "France",
+            country:  "france",
             year: 2020,
             veh_use_comm: 6598185,
             veh_use_pass: 44944450,
             veh_use_per_1000: 666.44,
         },
         {
-            country:  "Italy",
+            country:  "italy",
             year: 2020,
             veh_use_comm: 5281807,
             veh_use_pass: 44999681,
@@ -86,12 +86,12 @@ app.get(BASE_API_URL+"/in-use-vehicles/:country", (req,res) => {
         res.send(JSON.stringify(filteredIuv[0],null,2));
     }
 })
-/*
+
 app.get(BASE_API_URL+"/in-use-vehicles/:country/:year", (req,res) => {
-    var iuvCountry = req.params.name;
+    var iuvCountry = req.params.country;
     var iuvYear = req.params.year;
     filteredIuv = inUseVehicles.filter( (e) => {
-        return ((inUseVehicles.country == iuvCountry) && (inUseVehicles.year == iuvYear));
+        return ((e.country == iuvCountry) && (e.year == iuvYear));
     });
     if(filteredIuv == 0){
         res.sendStatus(404,"NOT FOUND");
@@ -100,7 +100,7 @@ app.get(BASE_API_URL+"/in-use-vehicles/:country/:year", (req,res) => {
         res.send(JSON.stringify(filteredIuv,null,2));
     }
 })
-*/
+
 
 app.post(BASE_API_URL+"/in-use-vehicles", (req,res) => {
     inUseVehicles.push(req.body);
