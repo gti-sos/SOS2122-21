5ab1e23a4e0fcb0bdc8ea7faa62e1308f2f7ea34
javrodmor1 <javrodmor1@alum.us.es>
2022-05-28 20:51:06 +0200

    Cambios busqueda

--- src/front/registrationsVehicles/RvTable.svelte
+++ src/front/registrationsVehicles/RvTable.svelte
@@ -1,272 +1,279 @@
 <script>
-    import { onMount } from "svelte";
-    import Table from "sveltestrap/src/Table.svelte";
-    import Button from "sveltestrap/src/Button.svelte";
-    import { Alert, Nav, NavItem, NavLink, Badge, Navbar, NavbarBrand,} from 'sveltestrap';
-    let fireData = [];
-    let newFire = {
-        country: "",
-        year: "",
-        fire_nfc: "",
-        fire_aee: "",
-        fire_nvs: "",
-    };
-    let limit = 10;
-    let offset = 0;
-    let page = (offset/10)+1;
-    let num_pages= 0;
-    let dialogMSG = "";
-    let color = "";
-    let findFireCountry = "";
-    let findFireYear = "";
-    let findFireNfc = "";
-    let findFireAee = "";
-    let findFireNvs = "";
-    onMount(getFireData);
-    //Método Get
-    async function getFireData() {
-        getNumPages()
-        const res = await fetch("/api/v2/fire-stats?limit="+limit+"&offset="+offset);
-        if (res.ok) {
-            const json = await res.json();
-            fireData = json;
-            page = (offset/10)+1
-        }
-    }
-    //Load initial data
-    async function loadInitialData() {
-        await fetch("/api/v1/registrations-vehicles/loadInitialData");
-        const res = await fetch("/api/v2/fire-stats?limit="+limit+"&offset="+offset);
-        if (res.ok) {
-            const json = await res.json();
-            fireData = json;
-            color = "success"
-            dialogMSG = "Se han cargado los datos iniciales";
-        }
-    }
-    //New fire
-    async function insertFireData() {
-        console.log("Insertando nuevo dato: "+newFire.country)
-        if (newFire.country == "" || newFire.country == null || 
-            newFire.year == "" || newFire.year == null ||
-            newFire.fire_aee == "" || newFire.fire_aee == null ||
-            newFire.fire_nfc == "" || newFire.fire_nfc == null || 
-            newFire.fire_nvs == "" || newFire.fire_nvs == null)
-        {
-            color = "danger"
-            dialogMSG = "Existe más de un campo vacío.";              
-        } else {
-            console.log("Nuevo incendio: "+newFire)
-            const res = await fetch("/api/v2/fire-stats", {
-                method: "POST",
-                body: JSON.stringify(newFire),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then(function (res) {
-                if (res.status == 201) {
-                    color = "success"
-                    dialogMSG = "Se ha añadido un nuevo incendio.";
-                }
-                else if (res.status == 409) {
-                    color = "danger"
-                    dialogMSG = "Datos introducidos incorrectos.";                
-                } else if (res.status == 400) {
-                    color = "danger"
-                    dialogMSG = "Ya existe un incendio con los mismos datos.";                
-                }
-            });
-        }
-        await getFireData()
-    }
-    //Delete an specif fire
-    async function deleteFire(country, year) {
-        const res = await fetch(
-            "/api/v2/fire-stats/" + country + "/" + year,
-            {
-                method: "DELETE",
-            }
-        ).then(function (res) {
-            if (res.status == 200) {
-                color = "success"
-                dialogMSG = "Se ha eliminado un incedio.";
-          
-            } else if (res.status == 404) {
-                color = "danger"
-                dialogMSG = "Se ha producido un error.";                  
-            }
-        });
-        await getFireData();
-    }
-    //Delete all fire data
-    async function deleteAllFire() {
-        const res = await fetch("/api/v2/fire-stats/", {
-            method: "DELETE",
-        }).then(function (res) {
-            if (res.ok) {
-                getFireData();
-                color = "success"
-                dialogMSG = "Se han eliminado todos los incendios.";
-            } else {
-                color = "danger"
-                dialogMSG = "Se ha producido un error.";
-            }
-        });
-        await getFireData();
-    }
-    //Find fire
-    async function findFire(findFireCountry, findFireYear, findFireNfc, findFireAee, findFireNvs){
-        if(typeof findFireCountry == 'undefined'){
-            findFireCountry = "";
-        }
-        if(typeof findFireYear == 'undefined' || findFireYear == "null"){
-            findFireYear = "";
-        }
-        if(typeof findFireNfc == 'undefined'){
-            findFireNfc = "";
-        }
-        if(typeof findFireAee=='undefined'){
-            findFireAee = "";
-        }
-        if(typeof findFireNvs == 'undefined'){
-            findFireNvs = "";
-        }
-        const res = await fetch("/api/v2/fire-stats?country="+findFireCountry+"&year="+findFireYear+"&fire_nfc="+findFireNfc+"&fire_aee="+findFireAee+"&fire_nvs="+findFireNvs);
-        if (res.ok){
-            const json = await res.json();
-            fireData = json;
-            if(fireData.length>0){
-                color = "success"
-                dialogMSG = "Dato(s) encontrado(s): " + fireData.length;
-            }
-        }else{
-            color = "danger";
-            dialogMSG = "No se ha encontrado ningún resultado.";
-        }
-    }
-    const nextPage = () => {offset+=10; getFireData()}
-    const previousPage = () => {offset-=10; getFireData()}
-    async function getNumPages() {
-        const res = await fetch("/api/v2/fire-stats");
-        let fires=[]
-        if(res.ok){
-            const json = await res.json();
-            fires = json;
-            num_pages=(fires.length/10)+1|0;
-            if(fires.length%10==0 && num_pages !== 1){
-                num_pages--;
-            }
-        }
-    }
+	import { onMount } from "svelte";
+	import { Alert, Col, Container, Row } from "sveltestrap";
+	import Table from "sveltestrap/src/Table.svelte";
+	import Button from "sveltestrap/src/Button.svelte";
+
+	const delay = ms => new Promise(res => setTimeout(res, ms));
+
+	let iuv = [];
+	let iuvCopy = [];
+
+	let estado = "";
+    let visibilidad = false;
+    let color = "danger";
+
+	let newIuv = {
+		country: "",
+		year: "",
+		veh_sale: "",
+		veh_per_1000: "",
+		variation: "",
+	};
+
+
+
+	var currentTime = new Date();
+	let yFrom = currentTime.getFullYear();
+	let yTo = currentTime.getFullYear();
+
+	let numPags = 0;
+	const limitPag = 6;
+	const defaultOffset = 0;
+	//onMount(getPags)
+	onMount(getIuv);
+
+	async function getPags(){
+		const res = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles");
+		if(res.ok){
+			const data = await res.json();
+			iuv = data;
+		}
+		numPags = Math.ceil(iuv.length/limitPag);
+	}
+	
+	async function getIuv(parametros="",busqueda=false) {
+		await getPags();
+		
+		console.log("Fetching data....");
+		let res;
+		let resBusqueda;
+		if(busqueda){
+			res = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles"+parametros+`&offset=${defaultOffset}&limit=${limitPag}`);
+			resBusqueda = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles"+parametros);
+		}
+		else{
+			res = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles"+parametros+`?offset=${defaultOffset}&limit=${limitPag}`);
+		}
+	
+		console.log(res.ok);
+
+		if (res.ok && busqueda) {
+			const data = await res.json();
+			const data2 = await resBusqueda.json();
+			iuv = data;
+			iuvCopy = data2;
+			numPags = Math.ceil(iuvCopy.length/limitPag)
+			visibilidad = true;
+			color="success";
+			estado="Búsqueda realizada correctamente";
+			for(let i=0; i<iuv.length ; i++){
+				let y = iuv[i].year;
+				if(y < yFrom){
+					yFrom = y;
+				}
+			}
+			console.log("Received data: " + iuv.length);
+		}
+		else if(res.ok && !busqueda){	
+			const data = await res.json();
+			iuv = data;
+			for(let i=0; i<iuv.length ; i++){
+				let y = iuv[i].year;
+				if(y < yFrom){
+					yFrom = y;
+				}
+			}
+			console.log("Numero paginas: "+numPags);
+			console.log("Received data: " + iuv.length);
+		}
+		else{
+			if(res.status == "400"){
+				visibilidad = true;
+				color="danger";
+				estado="No se puede realizar la búsqueda entre dichos años";
+				await getIuv();
+			}
+			if(res.status == "405"){
+				visibilidad = true;
+				color="danger";
+				estado="Método no permitido";
+			}
+			if(res.status == "404"){
+				visibilidad = true;
+				color="danger";
+				estado="Elemento no encontrado";
+				getIuv();
+			}
+			if(res.status == "500"){
+				visibilidad = true;
+				color="success";
+				estado="Error interno del servidor";
+				getIuv();
+			}
+		}
+	}
+
+	async function getIuvPag(offset){
+		let off = offset * limitPag;
+		const res = await fetch(`https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles?from=${yFrom}&to=${yTo}&offset=${off}&limit=${limitPag}`);
+		if(res.ok){
+			const data = await res.json();
+				iuv = data;
+				await delay(50);
+		}
+	}
+
+	async function insertIuv() {
+		console.log("Inserting data...." + JSON.stringify(newIuv));
+		if (newIuv.country == "" || 
+			newIuv.year == "" ||
+            newIuv.veh_sale == "" || 
+			newIuv.veh_per_1000 == "" || 
+			newIuv.variation == "" ){
+			visibilidad = true;
+			color="warning";
+			estado=`Ningún campo debe estar vacio`;
+		}
+		else{
+			const res = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles", {
+				method: "POST",
+				body: JSON.stringify(newIuv),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}).then(async function (res) {
+				if (res.ok){
+					newIuv.country="";
+					newIuv.year="";
+					newIuv.veh_sale="";
+					newIuv.veh_per_1000="";
+					newIuv.variation="";
+					getIuv();
+					visibilidad = true;
+					color="success";
+					estado="Registro añadido correctamente";
+				}
+				else{
+					if(res.status == "409"){
+						visibilidad = true;
+						color="danger";
+						estado=`Ya existe un registro con el país "${newIuv.country}" y el año "${newIuv.year}"`;
+					}
+				}
+			});
+			console.log("DONE");
+		}
+	}
+
+	async function deleteAll() {
+		console.log("Deleting all");
+		const res = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles", {
+			method: "DELETE",
+		}).then(async function (res) {
+			visibilidad = true;
+			getIuv();
+			color="success";
+			estado="Registros eliminados correctamente";
+		});
+	}
+
+	async function loadInitialData() {
+		console.log("Inserting default data");
+		await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles/loadInitialData").then(async function (res) {
+			getIuv();
+			visibilidad = true;
+			color="success";
+			estado="Registros añadidos correctamente";
+		});
+	}
+
+
+	async function deleteIuv(country,year){
+		const id = country + "/" + year;
+		const res = await fetch("https://sos2122-21.herokuapp.com/api/v1/registrations-vehicles/"+id, {
+			method: "DELETE",
+		}).then(async function (res) {
+			getIuv();
+			visibilidad = true;
+			color="success";
+			estado="Registro eliminado correctamente";
+		});
+	}
+
 </script>
 
 <main>
-    <Navbar color="light" light>
-        <NavbarBrand>Incendios</NavbarBrand>
-        <Nav>
-            <NavItem>
-                <NavLink href="#/">Inicio</NavLink> 
-            </NavItem>
-            <NavItem>
-                <NavLink href="#/fire/graph">Estadísticas</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href="#/graph">Estadísticas globales</NavLink> 
-            </NavItem>
-            <NavItem>
-                <NavLink href="#/fire/billboard">Estadísticas Billboard(D03)</NavLink> 
-            </NavItem>
-        </Nav>
-    </Navbar>
-    <hr>
-    <Alert {color} dismissible>
-        {#if dialogMSG.length>0}
-		    {dialogMSG}
-	    {/if}
-    </Alert>
-    <hr>
-    <h4 style="text-align:center"><strong>Búsqueda de incendios</strong></h4>
-    <Table>
-        <th>País</th>
-        <th>Año</th>
-        <th>Número de incendios</th>
-        <th>Estimación anual de emisiones</th>
-        <th>Número de especies vegetales afectadas</th>
-        <th>Acciones</th>
-        <tr>
-            <td><input id= "findCountryInput" type = "text" bind:value="{findFireCountry}"></td>
-            <td><input id= "findYearInput" type = "number" bind:value="{findFireYear}"></td>
-            <td><input id= "findNfcInput" type = "number" bind:value="{findFireNfc}"></td>
-            <td><input id= "findAeeInput" type = "number" bind:value="{findFireAee}"></td>
-            <td><input id= "findNvsInput" type = "number" bind:value="{findFireNvs}"></td>
-            <td>
-                <Button color="primary" on:click="{findFire (findFireCountry, findFireYear, findFireNfc, findFireNfc, findFireNvs)}">Buscar</Button>
-            </td>
+	<h1>Listado de vehículos en uso</h1>
+	{#await iuv}
+		loading
+	{:then iuv}
+		<Container>
+			<Row>
+				<Col xs="6" sm="4"></Col>
+    			<Col xs="6" sm="4">
+					<Alert color={color} isOpen={visibilidad} toggle={() => (visibilidad = false)}>
+						{estado}
+					</Alert>
+				</Col>
+			</Row>
+		</Container>
+		
+		<Table bordered>
+			<thead>
+				<tr align="center">
+					<th> País </th>
+					<th> Año </th>
+					<th> Vehículos comerciales en uso </th>
+					<th> Vehículos de pasajeros en uso </th>
+					<th> Vehículos en uso por 1000 habitantes </th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr />
+				{#each iuv as e}
+					<tr align="center">
+						<td><a href="#/registrations-vehicles/{e.country}/{e.year}">{e.country}</a></td>
+						<td>{e.year}</td>
+						<td>{e.veh_sale}</td>
+						<td>{e.veh_per_1000}</td>
+						<td>{e.variation}</td>
+						<td	><Button outline color="danger" on:click={deleteIuv(e.country,e.year)}>Eliminar</Button></td>
+					</tr>
+				{/each}
+				<tr align="center">
+					<td><input bind:value={newIuv.country} type="text" required/></td>
+					<td><input bind:value={newIuv.year} type="number" required/></td>
+					<td><input bind:value={newIuv.veh_sale}	type="number"	required/></td>
+					<td><input bind:value={newIuv.veh_per_1000} type="number" required/></td>
+					<td><input bind:value={newIuv.variation} type="number" required/></td>
+					<td	><Button outline color="primary" on:click={insertIuv}>Añadir</Button></td>
+				</tr>
+			</tbody>
+		</Table>
 
-        </tr>
-    </Table>
-    <hr>
-    <h4 style="text-align:center"><strong>Lista de incendios</strong></h4>
-    {#await fireData}
-        Cargando los incendios
-    {:then fireData}
-    <Table responsive>
-        <thead>
-            <tr>
-                <th>País</th>
-                <th>Año</th>
-                <th>Número de incendios</th>
-                <th>Estimación anual de emisiones</th>
-                <th>Número de especies vegetales afectadas</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each fireData as fire}
-                <tr>
-                    <td><a href="#/fire-stats/{fire.country}/{fire.year}">{fire.country}</a></td>
-                    <td>{fire.year}</td>
-                    <td>{fire.fire_nfc}</td>
-                    <td>{fire.fire_aee}</td>
-                    <td>{fire.fire_nvs}</td>
-                    <td> <a href="#/fire-stats/{fire.country}/{fire.year}"> <Button color="primary">Editar</Button></a></td>
-                    <td><Button color="danger" on:click={deleteFire(fire.country, fire.year)}>Eliminar</Button></td>
+		<div align="center">
+			{#each Array(numPags) as _,i}
+				<Button outline color="secondary" on:click={()=>{getIuvPag(i)}}>{i}</Button>&nbsp
+			{/each}
+		</div>
 
-                </tr>
-            {/each}
-            <tr>
-                <td><input id="newCountryInput" bind:value={newFire.country} /></td>
-                <td><input id="newYearInput" type=number bind:value={newFire.year} /></td>
-                <td><input id="newNfcInput" bind:value={newFire.fire_nfc} /></td>
-                <td><input id="newAeeInput" bind:value={newFire.fire_aee} /></td>
-                <td><input id="newNvsInput" bind:value={newFire.fire_nvs}/></td>
-                <td><Button color="primary" on:click={insertFireData}>Añadir</Button></td>
-            </tr>
-        </tbody>
-    </Table>
-    
-
-    <Button color="success" on:click={loadInitialData}>
-        Cargar datos inciales
-    </Button>
-    <Button color="danger" on:click={deleteAllFire}>
-        Eliminar todo
-    </Button>
-
-
-    {#if fireData.length !== 0}
-        <div style="text-align: center; " >
-            <h4>
-                {#if page != 1}
-                    <Button color="success" on:click={previousPage}>Anterior</Button>
-                {/if}
-                <Badge> {page} / {num_pages}</Badge>
-                {#if num_pages-page!=0 }
-                    <Button color="success" on:click={nextPage}>Siguiente</Button>
-                {/if}  
-            </h4>
-            
-        </div>
-    {/if}
-    {/await}
+		<Button color="danger" on:click={deleteAll}>ELIMINAR TODOS LOS REGISTROS</Button>
+		<Button outline color="primary" on:click={loadInitialData}>Importar registros por defecto</Button>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<h5>Buscar registros entre el año <input bind:value={yFrom} type="text"/> y el <input bind:value={yTo} type="text"/> 
+			<Button color="info" on:click={getIuv(`?from=${yFrom}&to=${yTo}`,true)}>Buscar</Button> 
+			<Button outline color="info" on:click={async () =>{
+				visibilidad = true;
+				color="success";
+				estado="Búsqueda limpiada correctamente";
+				getIuv(); 
+				}}>Limpiar búsqueda</Button> 
+		</h5>
+	{/await}
 </main>
